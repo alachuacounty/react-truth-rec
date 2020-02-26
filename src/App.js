d990feb09de9d33e05e4458b85117cda78e321c6
ashvini-patel <patel.mashvini@gmail.com>
2020-02-26 16:01:02 -0500

    Contact Page
    Added page and rendered form,
    working on client side validation in jsx

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Navigation from "./components/Navigation";
 import Modal from "./components/Modal"
 import Footer from "./components/Footer";
 import MainPage from "./pages";
-
 import EventsPage from "./pages/events";
+import ContactPage from "./pages/contact";
 
 
 //Import all needed Component for this tutorial
@@ -22,7 +22,7 @@ function App() {
                   {title:'Explore History', link: 'resources.html'},
                   {title:'Reparations', link: 'reparations.html'},
                   {title:'Events', link: '/events'},
-                  {title:'Contact', link: 'contact.aspx'},
+                  {title:'Contact', link: '/contact'},
                   {title:'Resources', link: 'resources.html'},
                   {title:'Timeline', link: 'timeline.html'},
                   {title:'Map', link: 'map.html'}
@@ -36,6 +36,7 @@ function App() {
       <Switch>
       <Route exact path="/"  render={(props) => <MainPage {...props} links={navLinks} />}/>
       <Route exact path="/events" component={EventsPage} />
+      <Route exact path="/contact" component={ContactPage} />
       </Switch>
     </Router>
     <Footer footertext="&copy; Alachua County Truth and Reconciliation"/>
---
+++ src/pages/contact.jsx
@@ -0,0 +1,196 @@
+import React from "react";
+import '../css/TruthRec.css';
+import "../css/contact.css";
+
+var hiddenStyle = { display: 'none'};
+
+function validate() { alert("not overwriiten");};
+
+class ContactPage extends React.Component {
+
+    componentDidMount() {
+        var field1 = document.getElementById("firstName");
+        var field2 = document.getElementById("lastName");
+        var field4 = document.getElementById("inputEmail");
+        var field5 = document.getElementById("inputMessage");
+
+        var validationMessages = document.getElementsByClassName("form-validation");
+
+
+        function isValid_Name(field) {
+
+            return field.value !== "";
+        }
+
+
+        function isValid_Email() {
+
+
+            if (field4.value === "") {
+                validationMessages[3].innerText = "* Required";
+                return false;
+            }
+            var email = field4.value;
+
+            const expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+
+            if (!expression.test(email.toLowerCase())) {
+                validationMessages[3].innerText = "Please specify a valid email address.";
+                return false;
+            }
+
+
+
+
+            return true;
+        }
+
+        function showFirstNameErrorMsg() {
+
+            if (field1.value === "") {
+                validationMessages[0].style["display"] = "inline-block";
+            }
+            else {
+                validationMessages[0].style["display"] = "none";
+            }
+
+        }
+        function showLastNameErrorMsg() {
+
+            if (field2.value === "") {
+                validationMessages[1].style["display"] = "inline-block";
+            }
+            else {
+                validationMessages[1].style["display"] = "none";
+            }
+
+        }
+
+        function showEmailErrorMsg() {
+            if (!isValid_Email()) {
+                validationMessages[3].style["display"] = "inline-block";
+            }
+            else {
+                validationMessages[3].style["display"] = "none";
+            }
+
+        }
+
+
+        function validate_form() {
+
+            alert("trying client side validatin");
+
+            var invalid = false;
+
+            field1 = document.getElementById("firstName");
+            field2 = document.getElementById("lastName");
+            field4 = document.getElementById("inputEmail");
+            field5 = document.getElementById("inputMessage");
+
+
+            if (!isValid_Email()) {
+                invalid = true;
+                showEmailErrorMsg();
+                field4.focus();
+            }
+            if (!isValid_Name(field2)) {
+                invalid = true;
+                showLastNameErrorMsg();
+                field2.focus();
+            }
+
+            if (!isValid_Name(field1)) {
+                invalid = true;
+                showFirstNameErrorMsg();
+                field1.focus();
+            }
+
+
+            field1.addEventListener("change", showFirstNameErrorMsg);
+            field2.addEventListener("change", showLastNameErrorMsg);
+            field4.addEventListener("change", showEmailErrorMsg);
+
+            if (!invalid) {
+                //flipcard();
+            }
+
+            return !invalid;
+
+        }
+
+
+        /*var prm = Sys.WebForms.PageRequestManager.getInstance();
+
+        prm.add_endRequest(function () {
+            // re-bind your jQuery events here
+
+
+            field1 = document.getElementById("firstName");
+            field2 = document.getElementById("lastName");
+            field4 = document.getElementById("inputEmail");
+            field5 = document.getElementById("inputMessage");
+
+            field1.addEventListener("change", showFirstNameErrorMsg);
+            field2.addEventListener("change", showLastNameErrorMsg);
+            field4.addEventListener("change", showEmailErrorMsg);
+        });*/
+
+
+        validate = validate_form;
+
+
+
+    }
+
+    render(){
+        return (
+            <section id="main-section" class="main">
+                <div id="main-content">
+                    <h1 class="title">Contact Us</h1>
+                    <div class="story">
+                        <h3><strong>Send Us Your Story</strong></h3>
+                    </div>                   
+                    <div class="form">
+                        <form method="post" action="validate_form()" id="contactform">
+                              <div id="UpdatePanel1">
+	
+                                        <ul class="form-style-1">
+                                            <li>
+                                                <label>Full Name <span class="required">*</span></label>
+                                                <input name="firstName" maxLength="125" id="firstName" type="text" class="field-divided" placeholder="First" required="required"/>
+                                                <span id="firstNameMsg" class="form-validation" aria-live="polite" aria-label="First Name"> * Required </span>
+                                                <input name="lastName" maxLength="125" id="lastName" type="text" class="field-divided" placeholder="Last" required="required"/>
+                                                <span id="lastNameMsg" class="form-validation" aria-live="polite" aria-label="Last Name"> * Required  </span>
+
+                                            </li>
+                                            <li>
+                                                <label htmlFor="inputEmail">Email <span class="required">*</span></label>
+                                                <input name="inputEmail" maxLength="256" id="inputEmail" type="email" class="field-long" placeholder="Email" required="required"/>
+                                                <span id="EmailMsg" class="form-validation" aria-live="polite" aria-label="Email"> Please specify a valid email address.  </span>
+                                            </li>
+                                            <li>
+                                                <label htmlFor="inputMessage">Your Message <span class="required">*</span></label>
+                                                <textarea name="inputMessage" rows="3" cols="20" id="inputMessage" class="field-long field-textarea" placeholder=""></textarea>
+
+                                            </li>
+                                            <li>
+                                                <input type="submit" name="contact_submit" value="Send Message" onClick={validate} id="contact_submit" class="btn learn-button text-white"/>
+                                            </li>
+                                            <li style={hiddenStyle}>
+                                                <label htmlFor="userComment">Comment </label>
+                                                <input name="userComment" autoComplete="off" id="userComment" type="text" class="form-control" placeholder="Your Comment" aria-label="Please leave this field empty"/>
+                                            </li>
+                                        </ul>                                 
+                                </div>
+                       </form>
+                    </div>
+             </div>  
+            </section>
+        );
+    }
+
+}
+
+
+export default ContactPage;
