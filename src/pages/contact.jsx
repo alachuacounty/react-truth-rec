a54425fd9f24d71a2a3947e47058fa1ddae80577
ashvini-patel <patel.mashvini@gmail.com>
2020-03-12 16:00:11 -0400

    Contact.jsx validation
    Field validation is completed
    
    Form validation pending

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -24,6 +24,7 @@ class ContactPage extends React.Component {
           fnameValid: false,
           lnameValid:false,
           emailValid:false,
+          msgValid:false,
           formValid:false
         }
 
@@ -41,148 +42,93 @@ class ContactPage extends React.Component {
             () => { this.validateField(name, value) } );
       }
 
+      validateField(fieldname, value){
+          // determine which field and call appropriate validate function 
 
-      validateField(){
-          
+          switch(fieldname){
+              case 'firstName': this.isValid_firstName(value);
+                                break;
+              case 'lastName': this.isValid_lastName(value);
+                                break;
+              case 'inputEmail': this.isValid_Email(value);
+                                break;
+              case 'inputMessage': this.isValid_Message(value);
+                                break;
+              default:
+                  break;
+          }
+
+          //set form valid to enable/disable form submission
+
+          let formValid = this.state.lnameValid && this.state.fnameValid && this.state.emailValid && this.state.msgValid;
+
+          this.setState({[this.state.formValid]: formValid});
+
       }
 
 
 
-    componentDidMount() {
-        var field1 = document.getElementById("firstName");
-        var field2 = document.getElementById("lastName");
-        var field4 = document.getElementById("inputEmail");
-        var field5 = document.getElementById("inputMessage");
-
-        var validationMessages = document.getElementsByClassName("form-validation");
+    isValid_firstName = (value) => {
 
 
-        function isValid_Name(field) {
+        let valid = value !== "";
 
-            return field.value !== "";
-        }
-
-
-        function isValid_Email() {
-
-
-            if (field4.value === "") {
-                validationMessages[3].innerText = "* Required";
-                return false;
-            }
-            var email = field4.value;
-
-            const expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
-
-            if (!expression.test(email.toLowerCase())) {
-                validationMessages[3].innerText = "Please specify a valid email address.";
-                return false;
-            }
+        this.setState({[this.state.fnameValid]: valid},
+        ()=> { this.updateErrorMsg(0,valid) });
+     
+    }
+  
+    isValid_lastName(value) {
 
 
+        let valid = value !== "";
 
-
-            return true;
-        }
-
-        function showFirstNameErrorMsg() {
-
-            if (field1.value === "") {
-                validationMessages[0].style["display"] = "inline-block";
-            }
-            else {
-                validationMessages[0].style["display"] = "none";
-            }
-
-        }
-        function showLastNameErrorMsg() {
-
-            if (field2.value === "") {
-                validationMessages[1].style["display"] = "inline-block";
-            }
-            else {
-                validationMessages[1].style["display"] = "none";
-            }
-
-        }
-
-        function showEmailErrorMsg() {
-            if (!isValid_Email()) {
-                validationMessages[3].style["display"] = "inline-block";
-            }
-            else {
-                validationMessages[3].style["display"] = "none";
-            }
-
-        }
-
-
-        function validate_form() {
-
-            alert("trying client side validatin");
-
-            var invalid = false;
-
-            field1 = document.getElementById("firstName");
-            field2 = document.getElementById("lastName");
-            field4 = document.getElementById("inputEmail");
-            field5 = document.getElementById("inputMessage");
-
-
-            if (!isValid_Email()) {
-                invalid = true;
-                showEmailErrorMsg();
-                field4.focus();
-            }
-            if (!isValid_Name(field2)) {
-                invalid = true;
-                showLastNameErrorMsg();
-                field2.focus();
-            }
-
-            if (!isValid_Name(field1)) {
-                invalid = true;
-                showFirstNameErrorMsg();
-                field1.focus();
-            }
-
-
-            field1.addEventListener("change", showFirstNameErrorMsg);
-            field2.addEventListener("change", showLastNameErrorMsg);
-            field4.addEventListener("change", showEmailErrorMsg);
-
-            if (!invalid) {
-                //flipcard();
-            }
-
-            return !invalid;
-
-        }
-
-
-        /*var prm = Sys.WebForms.PageRequestManager.getInstance();
-
-        prm.add_endRequest(function () {
-            // re-bind your jQuery events here
-
-
-            field1 = document.getElementById("firstName");
-            field2 = document.getElementById("lastName");
-            field4 = document.getElementById("inputEmail");
-            field5 = document.getElementById("inputMessage");
-
-            field1.addEventListener("change", showFirstNameErrorMsg);
-            field2.addEventListener("change", showLastNameErrorMsg);
-            field4.addEventListener("change", showEmailErrorMsg);
-        });*/
-
-
-        validate = validate_form;
-
-
+        this.setState({[this.state.lnameValid]: valid},
+        ()=> { this.updateErrorMsg(1,valid) });
 
     }
 
+    isValid_Email(value){
+        
+        //TODO set state valid for email
+        //regex match stuff here
+        const expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+        
+        let valid = (value === "" || !expression.test(value.toLowerCase()) ) ?false:true;
+
+        this.setState({[this.state.emailValid]: valid},
+            ()=> { this.updateErrorMsg(2,valid) });
+    
+    }
+
+
+    isValid_Message(value) {
+
+        
+        let valid = value !== "";
+
+        this.setState({[this.state.msgValid]: valid},
+        ()=> { this.updateErrorMsg(3,valid) });
+
+    }
+
+    updateErrorMsg(index, validity) {
+
+        console.log(index + ":: "+validity);
+
+        const validationMessages = document.getElementsByClassName("form-validation");
+
+        if (!validity) {
+            validationMessages[index].style["display"] = "inline-block";
+        }
+        else {
+            validationMessages[index].style["display"] = "none";
+        }
+
+    }
+
+  
+  
     render(){
         return (
             <section id="contact-main" class="main">
@@ -198,28 +144,28 @@ class ContactPage extends React.Component {
                                         <ul class="form-style-1">
                                             <li>
                                                 <label>Full Name <span class="required">*</span></label>
-                                                <input name="firstName" maxLength="125" id="firstName" type="text" class="field-divided" placeholder="First" required="required"/>
+                                                <input name="firstName" maxLength="125" id="firstName" type="text" class="field-divided" placeholder="First" required="required" onChange={this.handleUserInput} value={this.state.firstName} />
                                                 <span id="firstNameMsg" class="form-validation" aria-live="polite" aria-label="First Name"> * Required </span>
-                                                <input name="lastName" maxLength="125" id="lastName" type="text" class="field-divided" placeholder="Last" required="required"/>
+                                                <input name="lastName" maxLength="125" id="lastName" type="text" class="field-divided" placeholder="Last" required="required" onChange={this.handleUserInput} value={this.state.lastName}/>
                                                 <span id="lastNameMsg" class="form-validation" aria-live="polite" aria-label="Last Name"> * Required  </span>
 
                                             </li>
                                             <li>
                                                 <label htmlFor="inputEmail">Email <span class="required">*</span></label>
-                                                <input name="inputEmail" maxLength="256" id="inputEmail" type="email" class="field-long" placeholder="Email" required="required"/>
+                                                <input name="inputEmail" maxLength="256" id="inputEmail" type="email" class="field-long" placeholder="Email" required="required" onChange={this.handleUserInput} value={this.state.email}/>
                                                 <span id="EmailMsg" class="form-validation" aria-live="polite" aria-label="Email"> Please specify a valid email address.  </span>
                                             </li>
                                             <li>
                                                 <label htmlFor="inputMessage">Your Message <span class="required">*</span></label>
-                                                <textarea name="inputMessage" rows="3" cols="20" id="inputMessage" class="field-long field-textarea" placeholder=""></textarea>
-
+                                                <textarea name="inputMessage" rows="3" cols="20" id="inputMessage" class="field-long field-textarea" placeholder="" onChange={this.handleUserInput} value={this.state.inputMessage}></textarea>
+                                                <span id="MessageMsg" class="form-validation" aria-live="polite" aria-label="Message"> * Required </span>
                                             </li>
                                             <li>
-                                                <input type="submit" name="contact_submit" value="Send Message" onClick={this.validate} id="contact_submit" class="btn learn-button text-white"/>
+                                                <input type="submit" name="contact_submit" value="Send Message" id="contact_submit" class="btn learn-button text-white" disabled={!this.state.formValid}/>
                                             </li>
                                             <li style={hiddenStyle}>
                                                 <label htmlFor="userComment">Comment </label>
-                                                <input name="userComment" autoComplete="off" id="userComment" type="text" class="form-control" placeholder="Your Comment" aria-label="Please leave this field empty"/>
+                                                <input name="userComment" autoComplete="off" id="userComment" type="text" class="form-control" placeholder="Your Comment" aria-label="Please leave this field empty" onChange={this.handleUserInput} value={this.state.userComment}/>
                                             </li>
                                         </ul>                                 
                                 </div>
