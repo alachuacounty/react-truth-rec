69c8bfe08496689dfe09cf246340aefa4367f124
ashvini-patel <patel.mashvini@gmail.com>
2020-03-13 15:34:48 -0400

    contact form validation
    Added handleSubmit event
    Form validity relies on field validity and needs a fix for state updation

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -4,12 +4,6 @@ import "../css/contact.css";
 
 var hiddenStyle = { display: 'none'};
 
-function validate() { 
-    alert("not overwriiten");
-};
-
-
-
 class ContactPage extends React.Component {
 
 
@@ -28,9 +22,7 @@ class ContactPage extends React.Component {
           formValid:false
         }
 
-        //Binding submit event with constructor
-
-        //this.validate = this.handleUserInput;
+        this.handleSubmit = this.handleSubmit.bind(this);
 
 
       }
@@ -40,6 +32,45 @@ class ContactPage extends React.Component {
         const value = e.target.value;
         this.setState({[name]: value},
             () => { this.validateField(name, value) } );
+      }
+
+      handleSubmit(event){
+         event.preventDefault();
+
+         let formValid = this.state.fnameValid && this.state.lnameValid  && this.state.emailValid && this.state.msgValid;
+         alert("submitting form " + this.state.fnameValid + this.state.lnameValid  + this.state.emailValid + this.state.msgValid);
+
+      }
+
+
+      validateForm(){
+
+      
+
+        /*let formValid = this.state.lnameValid && this.state.fnameValid && this.state.emailValid && this.state.msgValid;
+
+        this.setState({[this.state.formValid]: formValid},
+          ()=>{ alert("form valid = "+ this.state.formValid)});
+
+          console.log('fname : ' + this.state.fnameValid);
+          console.log('lname : ' + this.state.lnameValid);
+          console.log('email : ' + this.state.emailValid);
+          console.log('msg : ' + this.state.msgValid);*/
+
+          //check state formvalid
+         // post if valid - >return state formvalid
+
+         let msg = 'fname : ' + this.state.fnameValid + "\n" +
+         'lname : ' + this.state.lnameValid + "\n" +
+         'email : ' + this.state.emailValid + "\n" +
+         'msg : ' + this.state.msgValid + "\n" +
+         'form'+ this.state.formValid;
+
+         alert(msg);
+
+
+         return false;
+
       }
 
       validateField(fieldname, value){
@@ -56,25 +87,20 @@ class ContactPage extends React.Component {
                                 break;
               default:
                   break;
-          }
-
-          //set form valid to enable/disable form submission
-
-          let formValid = this.state.lnameValid && this.state.fnameValid && this.state.emailValid && this.state.msgValid;
-
-          this.setState({[this.state.formValid]: formValid});
+          }     
 
       }
 
 
-
     isValid_firstName = (value) => {
 
-
         let valid = value !== "";
+        let formValid = valid && this.state.lnameValid  && this.state.emailValid && this.state.msgValid;
 
-        this.setState({[this.state.fnameValid]: valid},
-        ()=> { this.updateErrorMsg(0,valid) });
+        alert("setting state to  - "+ valid)
+
+        this.setState({[this.state.fnameValid]: valid, [this.state.formValid]: formValid},
+        ()=> { this.updateErrorMsg(0,valid) ; alert("after fname check :: "+ this.state.fnameValid +" form is :" +this.state.formValid)});
      
     }
   
@@ -82,8 +108,9 @@ class ContactPage extends React.Component {
 
 
         let valid = value !== "";
+        let formValid = valid && this.state.fnameValid  && this.state.emailValid && this.state.msgValid;
 
-        this.setState({[this.state.lnameValid]: valid},
+        this.setState({[this.state.lnameValid]: valid, [this.state.formValid]: formValid},
         ()=> { this.updateErrorMsg(1,valid) });
 
     }
@@ -94,9 +121,10 @@ class ContactPage extends React.Component {
         //regex match stuff here
         const expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
         
-        let valid = (value === "" || !expression.test(value.toLowerCase()) ) ?false:true;
+        let valid = (value === "" || !expression.test(value.toLowerCase()) ) ? false:true;
+        let formValid = valid && this.state.fnameValid  && this.state.lnameValid && this.state.msgValid;
 
-        this.setState({[this.state.emailValid]: valid},
+        this.setState({[this.state.emailValid]: valid, [this.state.formValid]: formValid},
             ()=> { this.updateErrorMsg(2,valid) });
     
     }
@@ -106,15 +134,14 @@ class ContactPage extends React.Component {
 
         
         let valid = value !== "";
+        let formValid = valid && this.state.fnameValid  && this.state.lnameValid && this.state.emailValid;
 
-        this.setState({[this.state.msgValid]: valid},
+        this.setState({[this.state.msgValid]: valid, [this.state.formValid]: formValid},
         ()=> { this.updateErrorMsg(3,valid) });
 
     }
 
     updateErrorMsg(index, validity) {
-
-        console.log(index + ":: "+validity);
 
         const validationMessages = document.getElementsByClassName("form-validation");
 
@@ -138,30 +165,30 @@ class ContactPage extends React.Component {
                         <h3><strong>Send Us Your Story</strong></h3>
                     </div>                   
                     <div class="form">
-                        <form method="post" action="validate_form()" id="contactform">
+                        <form method="post" onSubmit={this.handleSubmit} id="contactform">
                               <div id="UpdatePanel1">
 	
                                         <ul class="form-style-1">
                                             <li>
                                                 <label>Full Name <span class="required">*</span></label>
-                                                <input name="firstName" maxLength="125" id="firstName" type="text" class="field-divided" placeholder="First" required="required" onChange={this.handleUserInput} value={this.state.firstName} />
+                                                <input name="firstName" maxLength="125" id="firstName" type="text" class="field-divided" placeholder="First" /*required="required"*/ onChange={this.handleUserInput} value={this.state.firstName} />
                                                 <span id="firstNameMsg" class="form-validation" aria-live="polite" aria-label="First Name"> * Required </span>
-                                                <input name="lastName" maxLength="125" id="lastName" type="text" class="field-divided" placeholder="Last" required="required" onChange={this.handleUserInput} value={this.state.lastName}/>
+                                                <input name="lastName" maxLength="125" id="lastName" type="text" class="field-divided" placeholder="Last"  /*required="required"*/  onChange={this.handleUserInput} value={this.state.lastName}/>
                                                 <span id="lastNameMsg" class="form-validation" aria-live="polite" aria-label="Last Name"> * Required  </span>
 
                                             </li>
                                             <li>
                                                 <label htmlFor="inputEmail">Email <span class="required">*</span></label>
-                                                <input name="inputEmail" maxLength="256" id="inputEmail" type="email" class="field-long" placeholder="Email" required="required" onChange={this.handleUserInput} value={this.state.email}/>
+                                                <input name="inputEmail" maxLength="256" id="inputEmail" type="email" class="field-long" placeholder="Email"  /*required="required"*/ onChange={this.handleUserInput} value={this.state.email}/>
                                                 <span id="EmailMsg" class="form-validation" aria-live="polite" aria-label="Email"> Please specify a valid email address.  </span>
                                             </li>
                                             <li>
                                                 <label htmlFor="inputMessage">Your Message <span class="required">*</span></label>
-                                                <textarea name="inputMessage" rows="3" cols="20" id="inputMessage" class="field-long field-textarea" placeholder="" onChange={this.handleUserInput} value={this.state.inputMessage}></textarea>
+                                                <textarea name="inputMessage" rows="3" cols="20" id="inputMessage" class="field-long field-textarea" placeholder="" onChange={this.handleUserInput} value={this.state.inputMessage} /*required="required"*/></textarea>
                                                 <span id="MessageMsg" class="form-validation" aria-live="polite" aria-label="Message"> * Required </span>
                                             </li>
                                             <li>
-                                                <input type="submit" name="contact_submit" value="Send Message" id="contact_submit" class="btn learn-button text-white" disabled={!this.state.formValid}/>
+                                                <input type="submit" name="contact_submit" value="Send Message" id="contact_submit" class="btn learn-button text-white" />
                                             </li>
                                             <li style={hiddenStyle}>
                                                 <label htmlFor="userComment">Comment </label>
